6f700b116f7c70fdcc8bc0139b1d254e0d57cb51
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2025-05-07 22:26:24 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ⏱️ FocusFlow
+
+**FocusFlow** is a modern Pomodoro timer web application designed to help you stay focused and productive. Built with cutting-edge tools and a sleek UI, it leverages the Pomodoro technique to help you work in focused intervals with scheduled breaks.
+
+## 🔧 Tech Stack
+
+- **Frontend**: [Next.js](https://nextjs.org/) + [Tailwind CSS](https://tailwindcss.com/)
+- **Icons**: [Lucide React](https://lucide.dev/)
+- **Backend**: [Next.js API Routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) + [Node.js](https://nodejs.org/)
+- **Styling**: Utility-first, responsive, and minimal UI via Tailwind CSS
+
+## ✨ Features
+
+- ⏳ Customizable Pomodoro timer (Focus / Short Break / Long Break)
+- ⚙️ Minimal and distraction-free interface
+- ⚡ Fast and responsive design
+- 📦 Fully componentized and clean code structure
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google"
+import Navbar from "@/app/components/sections/navbar"
+
+export const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: "FocusFlow",
+  description: "A Pomodoro timer built with Next.js",
+  icons: {
+    icon: "/favicon.ico",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="background-video"
+      >
+        <source src="/bg-video.mp4" type="video/mp4" />
+      </video>
+      <div className="fixed inset-0 bg-black/40 z-[-1]" />
+
+        <Navbar />
+        {children}
+
+      </body>
+    </html>
+  );
+}
